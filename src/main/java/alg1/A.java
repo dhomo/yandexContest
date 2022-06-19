570a8a524c4c6be99f36da5b1126508748c1536c
dhomo <arhon.one@gmail.com>
2022-06-19 12:09:46 +0500

    убрал ненужный try

--- src/main/java/alg1/A.java
+++ src/main/java/alg1/A.java
@@ -1,31 +1,34 @@
 package alg1;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class A {
     public static void main(String[] args) throws IOException {
-        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
-            int result;
-            int code = Integer.parseInt(reader.readLine());
-            int interactor = Integer.parseInt(reader.readLine());
-            int checker = Integer.parseInt(reader.readLine());
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int result;
+        int code = Integer.parseInt(reader.readLine());
+        int interactor = Integer.parseInt(reader.readLine());
+        int checker = Integer.parseInt(reader.readLine());
 
-            result = switch (interactor) {
-                case (0):
-                    if (code != 0) yield 3;
-                    else yield checker;
-                case (1):
-                    yield checker;
-                case (4):
-                    if (code != 0) yield 3;
-                     else yield 4;
-                case (6):
-                    yield 0;
-                case (7):
-                    yield 1;
-                default:
-                    yield interactor;
-            };
-            System.out.println(result);
-        }
+        result = switch (interactor) {
+            case (0):
+                if (code != 0) yield 3;
+                else yield checker;
+            case (1):
+                yield checker;
+            case (4):
+                if (code != 0) yield 3;
+                else yield 4;
+            case (6):
+                yield 0;
+            case (7):
+                yield 1;
+            default:
+                yield interactor;
+        };
+        System.out.println(result);
+
     }
 }
